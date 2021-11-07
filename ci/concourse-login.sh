a225b3c3e53f1a691dfb1210b7837ae903fb5721
jaedle <dennis.jekubczyk@gmail.com>
2021-11-08 00:52:23 +0100

    move ci to common repository

--- ci/concourse-login.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu -o pipefail
-
-fly --target arm-images@ci.jaedle.de \
-  login \
-  --concourse-url=https://ci.jaedle.de \
-  --team-name arm-images
--- ci/set-pipeline.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu -o pipefail
-
-fly -t arm-images@ci.jaedle.de set-pipeline -p caddy-webdav -c pipeline.yml
